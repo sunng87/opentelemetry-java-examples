8d760ec20ff7951d1bce4b85b6f66b38234725e8
Ning Sun <sunng@protonmail.com>
2023-07-15 21:07:35 +0800

    chore: add example for writing metrics to otlp http endpoint

---
+++ metrics/README.md
@@ -0,0 +1,13 @@
+# metrics
+
+## build
+
+```shell
+gradle shadowJar
+```
+
+## run
+
+```
+java -cp build/libs/opentelemetry-examples-metrics-0.1.0-SNAPSHOT-all.jar io.opentelemetry.example.metrics.GaugeExample
+```
--- metrics/src/main/java/io/opentelemetry/example/metrics/GaugeExample.java
+++ metrics/src/main/java/io/opentelemetry/example/metrics/GaugeExample.java
@@ -1,8 +1,16 @@
 package io.opentelemetry.example.metrics;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_NAME;
+
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.exporter.otlp.http.metrics.OtlpHttpMetricExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
+import io.opentelemetry.sdk.resources.Resource;
+import java.time.Duration;
 
 /**
  * Example of using a Long Gauge to measure execution time of method. The gauge callback will get
@@ -11,14 +19,46 @@ import io.opentelemetry.api.metrics.Meter;
  */
 public final class GaugeExample {
 
-  public static void main(String[] args) {
-    Meter sampleMeter = GlobalOpenTelemetry.getMeter("io.opentelemetry.example.metrics");
+  static OpenTelemetry initOpenTelemetry() {
+    // Include required service.name resource attribute on all spans and metrics
+    Resource resource =
+        Resource.getDefault()
+            .merge(Resource.builder().put(SERVICE_NAME, "OtlpExporterExample").build());
 
+    OpenTelemetrySdk openTelemetrySdk =
+        OpenTelemetrySdk.builder()
+            .setMeterProvider(
+                SdkMeterProvider.builder()
+                    .setResource(resource)
+                    .registerMetricReader(
+                        PeriodicMetricReader.builder(
+                                OtlpHttpMetricExporter.builder()
+                                    .setEndpoint("http://127.0.0.1:4000/v1/otlp/v1/metrics")
+                                    .build())
+                            .setInterval(Duration.ofMillis(5000))
+                            .build())
+                    .build())
+            .buildAndRegisterGlobal();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(openTelemetrySdk::close));
+
+    return openTelemetrySdk;
+  }
+
+  public static void main(String[] args) throws Exception {
+    OpenTelemetry openTelemetry = initOpenTelemetry();
+
+    Attributes attrs = Attributes.builder().put("host", "testhost").build();
+
+    Meter sampleMeter = openTelemetry.getMeter("io.opentelemetry.example.metrics");
     sampleMeter
-        .gaugeBuilder("jvm.memory.total")
+        .gaugeBuilder("jvm.memory.free")
         .setDescription("Reports JVM memory usage.")
         .setUnit("byte")
-        .buildWithCallback(
-            result -> result.record(Runtime.getRuntime().totalMemory(), Attributes.empty()));
+        .buildWithCallback(result -> result.record(Runtime.getRuntime().freeMemory(), attrs));
+
+    while (true) {
+      Thread.sleep(2000);
+    }
   }
 }
